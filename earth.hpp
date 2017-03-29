ae249135e6c87339d401d90f0d8bc13d847181c1
Aditya Gupta <adityagupta1089@gmail.com>
2017-03-29 19:24:45 +0530

    Added timings

--- earth.hpp
+++ earth.hpp
@@ -4,33 +4,26 @@
 #include <math.h>
 
 class point {
-	double longitude;
-	double latitude;
-	static double radius;
+  double longitude;
+  double latitude;
+  static double radius;
+
 public:
-	point(double _latitude, double _longitude) {
-		longitude = _longitude * M_PI / 180.0;
-		latitude = _latitude * M_PI / 180.0;
-	}
+  point(double _latitude, double _longitude) {
+    longitude = _longitude * M_PI / 180.0;
+    latitude = _latitude * M_PI / 180.0;
+  }
 
-	static double distance(const point&, const point&);
-	static void set_radius(const double&);
+  static double distance(const point &, const point &);
+  static void set_radius(const double &);
 
-	double get_latitude_degrees() const {
-		return latitude * 180.0 / M_PI;
-	}
+  double get_latitude_degrees() const { return latitude * 180.0 / M_PI; }
 
-	double get_longitude_degrees() const {
-		return longitude * 180.0 / M_PI;
-	}
+  double get_longitude_degrees() const { return longitude * 180.0 / M_PI; }
 
-	double get_latitude_radians() const {
-		return latitude;
-	}
+  double get_latitude_radians() const { return latitude; }
 
-	double get_longitude_radians() const {
-		return longitude;
-	}
+  double get_longitude_radians() const { return longitude; }
 };
 
 #endif /*EARTH_HPP*/
--- main.cpp
+++ main.cpp
@@ -3,49 +3,96 @@
 #include <boost/geometry/algorithms/distance.hpp>
 #include <boost/geometry/core/cs.hpp>
 #include <iostream>
+#include <time.h>
 
 namespace bg = boost::geometry;
-typedef bg::model::point<double, 2, bg::cs::spherical_equatorial<bg::degree> >
+typedef bg::model::point<
+
+    double, 2, bg::cs::spherical_equatorial<bg::degree>
+
+    >
     spherical_point;
-typedef bg::model::point<double, 2, bg::cs::geographic<bg::degree> >
+typedef bg::model::point<
+
+    double, 2, bg::cs::geographic<bg::degree>
+
+    >
     geographic_point;
 
 int main() {
+  float t1 = 0;
+  float t2 = 0;
+  float t3 = 0;
+
+  clock_t time_ = 0;
+  double distance_ = 0;
+
   double earth_radius = 6371000.0;
   point::set_radius(earth_radius);
   /* Assuming earth to be sphere in all these, but this isn't the case*/
-
   /* distance 268 km.*/
   point delhi(77.1024, 28.7041), jaipur(75.7873, 26.9124);
   spherical_point _delhi(77.1024, 28.7041), _jaipur(75.7873, 26.9124);
   geographic_point __delhi(77.1024, 28.7041), __jaipur(75.7873, 26.9124);
+  time_ = clock();
+  distance_ = point::distance(delhi, jaipur) / 1000;
+  t1 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(1) Distance between Delhi and Jaipur is aproximately "
-            << point::distance(delhi, jaipur) / 1000 << " km\n";
+            << distance_ << " km\n";
+  time_ = clock();
+  distance_ = (distance(_jaipur, _delhi) * earth_radius) / 1000;
+  t2 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(2) Distance between Delhi and Jaipur is aproximately "
-            << (distance(_jaipur, _delhi) * earth_radius) / 1000 << " km\n";
+            << distance_ << " km\n";
+  time_ = clock();
+  distance_ = distance(__jaipur, __delhi) / 1000;
+  t3 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(3) Distance between Delhi and Jaipur is aproximately "
-            << distance(__jaipur, __delhi) / 1000 << " km\n";
+            << distance_ << " km\n";
 
   /* distance 11867 km.*/
   point new_york(-74.0059, 40.7128);
   spherical_point _new_york(-74.0059, 40.7128);
   geographic_point __new_york(-74.0059, 40.7128);
+  time_ = clock();
+  distance_ = point::distance(jaipur, new_york) / 1000;
+  t1 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(1) Distance between Jaipur and New York is aproximately "
-            << point::distance(jaipur, new_york) / 1000 << " km\n";
+            << distance_ << " km\n";
+  time_ = clock();
+  distance_ = (distance(_jaipur, _new_york) * earth_radius) / 1000;
+  t2 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(2) Distance between Jaipur and New York is aproximately "
-            << (distance(_jaipur, _new_york) * earth_radius) / 1000 << " km\n";
+            << distance_ << " km\n";
+  time_ = clock();
+  distance_ = distance(__jaipur, __new_york) / 1000;
+  t3 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(3) Distance between Jaipur and New York is aproximately "
-            << distance(__jaipur, __new_york) / 1000
-            << " km\n";
+            << distance_ << " km\n";
 
   /* distance 6646 km.*/
-  point  paris(2.3522, 48.8566), chicago(87.6298, 41.8781);
+  point paris(2.3522, 48.8566), chicago(87.6298, 41.8781);
   spherical_point _paris(2.3522, 48.8566), _chicago(87.6298, 41.8781);
   geographic_point __paris(2.3522, 48.8566), __chicago(87.6298, 41.8781);
+  time_ = clock();
+  distance_ = point::distance(paris, chicago) / 1000;
+  t1 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(1) Distance between Paris and Chicago is aproximately "
-            << point::distance(paris, chicago) / 1000 << " km\n";
+            << distance_ << " km\n";
+  time_ = clock();
+  distance_ = (distance(_paris, _chicago) * earth_radius) / 1000;
+  t2 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(2) Distance between Paris and Chicago is aproximately "
-            << (distance(_paris, _chicago) * earth_radius) / 1000 << " km\n";
+            << distance_ << " km\n";
+  time_ = clock();
+  distance_ = distance(__paris, __chicago) / 1000;
+  t3 += float(clock() - time_) / CLOCKS_PER_SEC;
   std::cout << "(3) Distance between Paris and Chicago is aproximately "
-            << distance(__paris, __chicago) / 1000 << " km\n";
+            << distance_ << " km\n";
+  t1 *= 1000;
+  t2 *= 1000;
+  t3 *= 1000;
+  std::printf("The time taken are %0.2f, %0.2f and %0.2f ms.\n", t1, t2, t3);
+
+  return 0;
 }
